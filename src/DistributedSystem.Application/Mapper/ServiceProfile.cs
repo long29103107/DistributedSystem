92ad35d2800260a132a3022aedb0fbfc98ca690e
long.nguyen <long.nguyen@siliconstack.com.au>
2024-07-30 15:23:03 +0700

    update code

---
+++ src/DistributedSystem.Application/Mapper/ServiceProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DistributedSystem.Contract.Abstractions.Shared;
+using MassTransit;
+namespace DistributedSystem.Application.Mapper;
+
+public class ServiceProfile : Profile
+{
+    public ServiceProfile()
+    {
+        // V1
+        //CreateMap<Product, Response.ProductResponse>().ReverseMap();
+        //CreateMap<PagedResult<Product>, PagedResult<Response.ProductResponse>>().ReverseMap();
+
+        //// V2
+        //CreateMap<Product, Contract.Services.V2.Product.Response.ProductResponse>().ReverseMap();
+    }
+}
