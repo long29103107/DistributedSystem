1b7588f568329fa2be948c7216b143da34a04d1f
long.nguyen <long.nguyen@siliconstack.com.au>
2024-07-30 10:54:29 +0700

    first commit

---
+++ src/DistributedSystem.Presentation/AssemblyReference.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace KraftHeinz.PromotionPlanning.Service;
+
+public static class AssemblyReference
+{
+    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+    public static readonly string AssemblyName = typeof(AssemblyReference).Assembly.GetName().Name;
+}
